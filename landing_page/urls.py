f4a4e3d6ff8ef104263b6a3a3941b4948968dbe3
Neev Patel <neevpatel2600@gmail.com>
2025-08-22 16:48:14 +0530

    frontend for portfolio

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='landing_home'),
+    path('', views.home, name='home'),  # this will be the default "/"
 ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -1,7 +1,23 @@
+from newsapi import NewsApiClient
+import datetime
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
 def home(request):
-    return HttpResponse("Welcome to the Landing Page!")
+    API_KEY = "e10089beab6045d8aa9c685a108ee279"   # 🔑 replace with your valid key
+
+    newsapi = NewsApiClient(api_key=API_KEY)
+
+    today = datetime.date.today()
+    one_week_ago = today - datetime.timedelta(days=7)
+
+    all_articles = newsapi.get_everything(
+        q="bitcoin",
+        from_param=one_week_ago.strftime("%Y-%m-%d"),
+        to=today.strftime("%Y-%m-%d"),
+        language="en",
+        sort_by="relevancy",
+        page=1
+    )
+
+    articles = all_articles.get("articles", [])[:10]
+    
+    return render(request, "landing_page_1.html", {"articles": articles})
